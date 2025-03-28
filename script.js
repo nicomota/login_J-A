7ecdcf9ec1c962049817e214d14b84f3f79f8e4d
Nicolas Mota <nicolas.mota2000@gmail.com>
2025-03-28 15:39:25 -0300

    versao_final

---
+++ script.js
@@ -0,0 +1,69 @@
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.querySelector("form");
+let textForm = document.getElementById("textForm");
+let textEmail = document.getElementById("textEmail");
+let textPassword = document.getElementById("textPassword");
+
+form.addEventListener("submit", (e) => {
+  if (email.value == "" && password.value == "") {
+    textForm.textContent = "Você precisa preencher todos os campos!";
+  } else if (
+    validatorEmail(email.value) === true &&
+    validatorPassword(password.value) === true
+  ) {
+    console.log(email.value);
+    console.log(password.value);
+    textForm.textContent = "";
+    textEmail.textContent = "";
+    textPassword.textContent = "";
+  } else {
+    console.log("Requisição não atendida");
+  }
+
+  e.preventDefault();
+});
+
+email.addEventListener("keyup", () => {
+  if (validatorEmail(email.value) !== true) {
+	textEmail.textContent = "O formato do email deve ser EX: name@gmail.com"
+} else {
+    textEmail.textContent = "";
+  }
+});
+
+password.addEventListener("keyup", () => {
+  if (validatorPassword(password.value) !== true) {
+    textPassword.textContent =
+	textPassword.textContent = "Necessário 1 caracter especial, 1 número e uma letra maiúscula.";
+} else {
+    textPassword.textContent = "";
+  }
+});
+
+function validatorEmail(email) {
+  let emailPattern =
+    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  return emailPattern.test(email);
+}
+
+function validatorPassword(password) {
+  let passwordPattern =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return passwordPattern.test(password);
+}
+
+
+function logar(){
+
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+
+	if(email =="teste@gmail.com" && password == "@Teste123"){
+		alert('Login Realizado');
+		location.href = "";
+	}else{
+		alert('Usuario ou senha incorretos');
+	}
+
+}
